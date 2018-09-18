a9602132d1f9d745e285c083a9783979ff6f7ed1
vlon <viktor.lonnroth@ha.ax>
2018-09-18 09:18:59 +0300

    offset added

--- velvet_wind_sensor/velvet_wind_sensor.ino
+++ velvet_wind_sensor/velvet_wind_sensor.ino
@@ -3,6 +3,7 @@ int analogPin = 1;
 int value = 0;  
 float angle_in_degree = 0;
 char dataString[8] = {0};
+int offset = 143+180; //in degree
 
 void setup() {
   Serial.begin(9600);
@@ -15,7 +16,7 @@ void loop() {
   Serial.println(value);
   angle_in_degree = 360.0*value/1024.0;  //analogRead ouput is from 0 to 1023
   Serial.print("Value_in_degree: ");
-  Serial.println(angle_in_degree);
+  Serial.println(((int)(angle_in_degree+0.5) - offset)%360);
 
   sprintf(dataString,"%02X",angle_in_degree); // convert a value to hexa 
   Serial.println(dataString);   // send the data
